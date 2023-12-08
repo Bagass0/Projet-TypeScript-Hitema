c88d913f7fae215b4ada47821f2246b62e068004
Hugo <80489889+Bagass0@users.noreply.github.com>
2023-12-08 14:31:15 +0100

    first commit

---
+++ Exo1/exo.ts
@@ -0,0 +1,37 @@
+export type User = {
+    name: string;
+    age: number;
+    occupation: string;
+    compentences?: string[]; // Ajout du point d'interrogation pour indiquer que la propriété est facultative
+    adresse?: {
+      rue: string;
+      cp: number;
+      ville: string;
+    };
+  };
+  
+  export const users: User[] = [
+    {
+      name: 'Alain',
+      age: 25,
+      occupation: 'Boulanger',
+      compentences: ["js", "node"]
+    },
+    {
+      name: 'Béatrice',
+      age: 23,
+      occupation: 'Astronaute',
+      adresse: {
+        rue: "75 rue de Paris",
+        cp: 75000,
+        ville: "Paris"
+      }
+    }
+  ];
+  
+  export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+  }
+  
+  users.forEach(logPerson);
+  
