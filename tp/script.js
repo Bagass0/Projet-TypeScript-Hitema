32c8f35c1a64a6253e82476aea2afda84138b73b
Hugo <80489889+Bagass0@users.noreply.github.com>
2023-12-15 14:29:09 +0100

    import 2eme machine

--- tp/script.js
+++ tp/script.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,72 +45,95 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-// Fonction pour effectuer une requête AJAX
-function fetchData(url) {
+function getUsers() {
     return __awaiter(this, void 0, void 0, function () {
         var response;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0: return [4 /*yield*/, fetch(url)];
+                case 0: return [4, axios.get('https://jsonplaceholder.typicode.com/users')];
                 case 1:
                     response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2: return [2 /*return*/, _a.sent()];
+                    return [2, response.data];
             }
         });
     });
 }
-// Fonction pour fusionner et injecter les données dans le DOM
-function renderData(users, posts) {
-    // Exemple de logique de fusion : concaténation des noms des utilisateurs et des titres des messages
-    var mergedData = users.map(function (user, index) {
-        return "".concat(user.name, " - ").concat(posts[index].title);
-    }).join('<br>');
-    // Injectez les données dans le DOM
-    var appContainer = document.getElementById('app');
-    if (appContainer) {
-        appContainer.innerHTML = mergedData;
-    }
-}
-// Fonction pour filtrer les résultats
-function filterResults(keyword) {
-    // Implémentez la logique de filtrage ici
-}
-// Appel des requêtes AJAX
-function loadData() {
+function getPosts() {
     return __awaiter(this, void 0, void 0, function () {
-        var users, posts, error_1;
+        var response;
         return __generator(this, function (_a) {
             switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetchData('https://jsonplaceholder.typicode.com/users')];
+                case 0: return [4, axios.get('https://jsonplaceholder.typicode.com/posts')];
                 case 1:
-                    users = _a.sent();
-                    return [4 /*yield*/, fetchData('https://jsonplaceholder.typicode.com/posts')];
-                case 2:
-                    posts = _a.sent();
-                    renderData(users, posts);
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_1 = _a.sent();
-                    console.error('Erreur lors du chargement des données :', error_1);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
+                    response = _a.sent();
+                    return [2, response.data];
             }
         });
     });
 }
-// Appel de la fonction loadData au chargement de la page
-document.addEventListener('DOMContentLoaded', function () {
-    loadData();
-});
-// Écouteur d'événement pour le bouton de recherche
-document.addEventListener('click', function (event) {
-    var target = event.target;
-    if (target.id === 'searchButton') {
-        var searchInput = document.getElementById('searchInput');
-        var keyword = searchInput.value.trim().toLowerCase();
-        filterResults(keyword);
-    }
-});
+function displayData(usersWithPosts) {
+    return __awaiter(this, void 0, void 0, function () {
+        var container;
+        return __generator(this, function (_a) {
+            container = document.getElementById('user-posts');
+            container.innerHTML = '';
+            usersWithPosts.forEach(function (user) {
+                var userColumn = document.createElement('div');
+                userColumn.className = 'col-md-4 card-column';
+                var userCard = document.createElement('div');
+                userCard.className = 'card';
+                userCard.innerHTML = "\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">".concat(user.name, "</h5>\n        <h6 class=\"card-text text-secondary\">").concat(user.email, "</h6>\n        <p class=\"text-warning\">Titre des articles r\u00E9dig\u00E9s:</p>\n        <ul>\n          ").concat(user.posts.map(function (post) { return "<li>".concat(post.title, "</li>"); }).join(''), "\n        </ul>\n      </div>\n    ");
+                userColumn.appendChild(userCard);
+                container.appendChild(userColumn);
+            });
+            return [2];
+        });
+    });
+}
+function loadData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var _a, users, posts_1, usersWithPosts, error_1;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    _b.trys.push([0, 2, , 3]);
+                    return [4, Promise.all([getUsers(), getPosts()])];
+                case 1:
+                    _a = _b.sent(), users = _a[0], posts_1 = _a[1];
+                    usersWithPosts = users.map(function (user) { return (__assign(__assign({}, user), { posts: posts_1.filter(function (post) { return post.userId === user.id; }) })); });
+                    displayData(usersWithPosts);
+                    return [3, 3];
+                case 2:
+                    error_1 = _b.sent();
+                    console.error('There was an error fetching the data', error_1);
+                    return [3, 3];
+                case 3: return [2];
+            }
+        });
+    });
+}
+function filterData() {
+    return __awaiter(this, void 0, void 0, function () {
+        var titleInput, authorInput, titleQuery, authorQuery, _a, users, posts, filteredPosts, usersWithFilteredPosts;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    titleInput = document.getElementById('search-title');
+                    authorInput = document.getElementById('search-author');
+                    titleQuery = titleInput.value.toLowerCase();
+                    authorQuery = authorInput.value.toLowerCase();
+                    return [4, Promise.all([getUsers(), getPosts()])];
+                case 1:
+                    _a = _b.sent(), users = _a[0], posts = _a[1];
+                    filteredPosts = posts.filter(function (post) { return post.title.toLowerCase().includes(titleQuery); });
+                    usersWithFilteredPosts = users
+                        .map(function (user) { return (__assign(__assign({}, user), { posts: filteredPosts.filter(function (post) { return post.userId === user.id; }) })); })
+                        .filter(function (user) { return user.name.toLowerCase().includes(authorQuery) && user.posts.length > 0; });
+                    displayData(usersWithFilteredPosts);
+                    return [2];
+            }
+        });
+    });
+}
+document.getElementById('search-button').addEventListener('click', filterData);
+loadData();
